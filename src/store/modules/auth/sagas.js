979e954ed37262c585ae9a3ecc0710d7f448ad62
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-07 14:24:52 -0300

    app register

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -16,7 +17,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.log('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
 
@@ -24,8 +25,30 @@ export function* signIn({ payload }) {
 
     history.push('dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
