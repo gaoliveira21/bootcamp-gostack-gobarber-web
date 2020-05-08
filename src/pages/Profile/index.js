8d5472be9eb95aa527213c64c29ac02d0299e1ef
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-08 14:22:21 -0300

    profile styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Profile() {
-  return <div />;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input
+          name="email"
+          type="password"
+          placeholder="Seu endereço de e-mail"
+        />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
 
 export default Profile;
